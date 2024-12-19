9c937a07566cd157b44cee116938d0e22e4be130
michaele490 <michaelegharevba490@gmail.com>
2024-12-19 14:10:17 +0000

    Movies scaffold complete

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show edit update destroy]
+
+  def index
+    @movies = current_user.movies
+  end
+
+  def show
+  end
+
+  def new
+    @movie = current_user.movies.build
+  end
+
+  def create
+    @movie = current_user.movies.build(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_url, notice: "Movie was successfully deleted."
+  end
+
+  private
+
+  def set_movie
+    @movie = current_user.movies.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :genre)
+  end
+end
